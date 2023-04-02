75aa09ccdacfc3853fb0a9d254da6983967e0bbc
davidpanderson <davea@ssl.berkeley.edu>
2023-04-01 18:03:57 -0700

    Helps/Faure: tweaks.  Pretty good now.

--- examples/helps_faure.py
+++ examples/helps_faure.py
@@ -155,54 +155,63 @@ soprano_v0 = vol('meas4/4 \
     |33 meas4/4 [ p 1/1 mf 1/1 p 1/1 mp 1/1 p \
     |37 p 2/1 mf 1/1 mp \
     |40 meas2/4 mp 2/4 f \
-    |41 meas4/4 f 1/1 mf 1/1 f 2/1 p \
-    |45 p 5/1 pp \
-    |50 meas2/4 pp 2/4 p \
-    |51 meas4/4 p 1/1 mf 1/1 p 1/1 mp 1/1 pp \
+    |41 meas4/4 f 1/1 mf 1/1 f 2/1 mp \
+    |45 mp 5/1 mm \
+    |50 meas2/4 mm 2/4 mm \
+    |51 meas4/4 [ p 1/1 mf 1/1 p 1/1 mp 1/1 pp \
     |55 [ p 1/1 mf 1/1 p 1/1 mp 1/1 mf \
-    |59 mf 4/1 ppp \
+    |59 mf 1/1 mf_ 3/1 ppp \
     |63 [ p 4/1 ppp \
     |67 \
 ')
 alto_v0 = vol('meas4/4 \
     |1 ppp 31/1 ppp \
-    |32 meas2/4 [ ppp 2/4 ppp \
-    |33 meas4/4 *7 ppp 2/4 pp 2/4 ppp * \
-    |40 meas2/4 ppp 2/4 p \
-    |41 meas4/4 p 1/1 pp 1/1 mp 2/1 ppp \
-    |45 ppp 4/1 ppp \
+    |32 meas2/4 [ ppp 2/4 pppp \
+    |33 meas4/4 *7 pppp 2/4 _pp 2/4 pppp * \
+    |40 meas2/4 pppp 2/4 p \
+    |41 meas4/4 *6 [ pppp 2/4 _pp 2/4 pppp * \
+    |47 [ ppp 2/1 ppp \
     |49 ppp 2/4 p 2/4 ppp \
     |50 meas2/4 ppp 2/4 ppp \
     |51 meas4/4 \
 ')
 bass_v0 = vol('meas4/4 \
-    |1 *31 pp 2/4 p 2/4 pp * \
+    |1 *31 _pp 2/4 pp_ 2/4 _pp * \
     |32 meas2/4 [ pp 2/4 pp \
-    |33 meas4/4 *7 pp 2/4 p 2/4 pp * \
-    |40 meas2/4 pp 2/4 pp \
+    |33 meas4/4 *7 [ ppp 2/4 pp 2/4 ppp * \
+    |40 meas2/4 [ pp 2/4 pp \
     |41 meas4/4 *9 pp 2/4 p 2/4 pp * \
     |50 meas2/4 pp 2/4 pp \
     |51 meas4/4 [ ppp 4/1 ppp \
     |55 ppp 2/1 ppp 2/1 mf \
     |59 mf 4/1 ppp \
-    |63 [ p 4/1 ppp \
+    |63 [ pp 4/1 pppp \
     |67 \
 ')
 
-bass_v1 = accents('meas4/4 \
-    |1 *62 mm 1/8 p 1/8 mp 1/8 p 1/8 * \
+# mostly slight accents on quarter notes
+bass_accents = accents('meas4/4 \
+    |1 *62 mf 1/4 _mf 1/4 * \
+    |32 meas2/4 mf 1/4 _mf 1/4 \
+    |33 meas4/4 *14 mf 1/4 _mf 1/4 * \
+    |40 meas2/4 mf 1/4 mf 1/4  \
+    |41 meas4/4 *12 mf 1/4 _mf 1/4 * \
+    |47 *6 f 1/4 f 1/4 * \
+    |50 meas2/4 2/4 \
+    |51 meas4/4 *32 mf 1/4 _mf 1/4 * \
+    |67 \
 ')
 
 # tempo
 t0 = tempo('meas4/4 \
-    |1 *3 \
-        55 1/1 65 1/1 55 60 1/1 60 1/1 50 \
-        * \
-    |13 55 1/1 65 1/1 55 60 1/1 60 1/1 40 \
-    |17 *3 \
-        55 1/1 65 1/1 55 60 1/1 60 1/1 50 \
-        * \
-    |29 55 1/1 65 1/1 55 60 1/1 50 \
+    |1 55 1/1 65 1/1 45 60 1/1 60 1/1 50 \
+    |5 55 1/1 65 1/1 45 60 1/1 60 1/1 45 \
+    |9 55 1/1 65 1/1 45 60 1/1 60 1/1 50 \
+    |13 55 1/1 65 1/1 45 60 2/1 45 \
+    |17 55 1/1 65 1/1 45 60 1/1 60 1/1 50 \
+    |21 55 1/1 65 1/1 45 60 1/1 60 1/1 45 \
+    |25 55 1/1 65 1/1 45 60 1/1 60 1/1 50 \
+    |29 55 1/1 65 1/1 45 60 1/1 50 \
     |32 meas2/4 2/4 40 \
     |33 meas4/4 55 1/1 65 1/1 55 60 1/1 60 1/1 50 \
     |37 55 1/1 65 1/1 55 60 1/1 60 \
@@ -210,10 +219,10 @@ t0 = tempo('meas4/4 \
     |41 meas4/4 *2 \
         55 1/1 65 1/1 55 60 1/1 60 1/1 50 \
         * 50 1/1 40 \
-    |50 meas2/4 40 2/4 30 \
-    |51 meas4/4 55 1/1 65 1/1 55 60 1/1 60 1/1 30 \
+    |50 meas2/4 40 2/4 35 \
+    |51 meas4/4 55 1/1 65 1/1 55 60 2/1 45 \
     |55 55 1/1 65 1/1 55 60 1/1 60 1/1 50 \
-    |59 55 1/1 65 1/1 55 60 1/1 60 1/1 30 \
+    |59 55 1/1 65 1/1 55 60 2/1 40 \
     |63 55 1/1 65 1/1 55 1/1 40 1/1 30 \
     |67 \
 ')
@@ -223,43 +232,45 @@ dt1 = .05
 dt2 = .08
 dt3 = .11
 dt4 = .14
+dt5 = .18
 
 pauses = tempo(f'meas4/4 \
     |1 *7 \
-        p{dt2} 1/1 . {dt1}p{dt2} 2/1 . {dt0}p 1/1 . \
+        {dt5}p{dt5} 1/1 . {dt2}p{dt2} 2/1 . {dt2}p 1/1 . \
         * \
-    |29 p{dt2} 1/1 . {dt1}p{dt2} 2/1 . \
-    |32 meas2/4 {dt0}p 2/4 . \
-    |33 meas4/4 p{dt2} 1/1 . {dt1}p{dt2} 2/1 . {dt0}p 1/1 . \
-    |37 p{dt2} 1/1 . {dt1}p{dt2} 2/1 . \
+    |29 p{dt4} 1/1 . {dt2}p{dt2} 2/1 . \
+    |32 meas2/4 {dt1}p 2/4 . \
+    |33 meas4/4 {dt5}p{dt5} 1/1 . {dt2}p{dt2} 2/1 . {dt2}p 1/1 . \
+    |37 {dt5}p{dt5} 1/1 . {dt2}p{dt2} 2/1 . \
     |40 meas2/4 {dt0}p 2/4 . \
     |41 meas4/4 *2 \
-        p{dt2} 1/1 . {dt1}p{dt2} 2/1 . {dt0}p 1/1 . \
+        {dt5}p{dt5} 1/1 . {dt2}p{dt2} 2/1 . {dt2}p 1/1 . \
         * \
     |49 p{dt2} 1/1 . \
     |50 meas2/4 {dt0}p 2/4 . \
-    |51 meas4/4 *4 \
-        p{dt2} 1/1 . {dt1}p{dt2} 2/1 . {dt0}p 1/1 . \
-        * \
-    |67 \
+    |51 meas4/4 \
+        {dt3}p{dt5} 1/1 . {dt2}p{dt2} 2/1 . {dt2}p 1/1 . \
+        {dt5}p{dt5} 1/1 . {dt2}p{dt2} 2/1 . {dt2}p 1/1 . \
+        {dt5}p{dt5} 1/1 . {dt2}p{dt2} 2/1 . {dt2}p 1/1 . \
+    |63 {dt5}p{dt5} 1/1 . {dt3}p{dt2} 2/1 . {dt4}p 1/2 . {dt3}p 1/2 . \
+    |67 2p \
 ')
 
 # agogic accents in melody
-
+# these aren't used when the alto voice is active
 sop_shift = shift(f'meas4/4 \
-    |1 *7 \
-        {dt2} 1/1 {dt1} 1/1 1/1 {dt2} 1/1 \
+    |1 *4 \
+        {dt2} 1/1 {dt1} 1/1 1/1 {dt1} 1/1 \
         * \
-    |29 {dt2} 1/1 {dt1} 1/1 {dt2} 1/1 \
-    |32 meas2/4 {dt0} 2/4 \
-    |33 meas4/4 {dt2} 1/1 {dt1} 1/1 1/1 {dt2} 1/1 \
-    |37 {dt2} 1/1 {dt1} 1/1 {dt2} 1/1 \
-    |40 meas2/4 {dt0} 2/4 \
-    |41 meas4/4 *2 \
-        {dt2} 1/1 {dt1} 1/1 1/1 {dt2} 1/1 \
-        * \
-    |49 {dt2} 1/1 \
-    |50 meas2/4 {dt0} 2/4 \
+    |17 12/1 \
+    |29 3/1 \
+    |32 meas2/4 2/4 \
+    |33 meas4/4 4/1 \
+    |37 3/1 \
+    |40 meas2/4 2/4 \
+    |41 meas4/4 8/1 \
+    |49 1/1 \
+    |50 meas2/4 2/4 \
     |51 meas4/4 *4 \
         {dt2} 1/1 {dt1} 1/1 1/1 {dt2} 1/1 \
         * \
@@ -280,16 +291,16 @@ bass_ped = pedal('meas4/4 \
 nuance = True
 
 def main():
-    ns = Score(tempo=80)
+    ns = Score(tempo=84)
     if nuance:
         soprano.vol_adjust_pft(soprano_v0)
         alto.vol_adjust_pft(alto_v0)
         bass.vol_adjust_pft(bass_v0)
-        bass.vol_adjust_pft(bass_v1)
+        bass.vol_adjust_pft(bass_accents)
         bass.vsustain_pft(bass_ped)
     ns.append_score([
-        #soprano,
-        #alto,
+        soprano,
+        alto,
         bass
     ])
     if nuance:
