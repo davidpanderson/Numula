58496212f1ede0820101c0d10c009961581cbafd
davidpanderson <davea@ssl.berkeley.edu>
2024-11-10 12:41:13 -0800

    surf

--- comp/surf.py
+++ comp/surf.py
@@ -1,10 +1,13 @@
-import random
+import random, math
 
 from numula.nuance import *
 
 def surf():
     s = Score()
     for i in range(1000):
-        n = Note(i/10., 1, random.randint(20, 40), .5)
+        v = (1-math.cos(i/30.))/2
+        v = v*v
+        v = .2+.8*v
+        n = Note(i/40., .2, random.randint(20, 40), v)
         s.insert_note(n)
     return s
