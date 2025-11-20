4a9845e9f29d922937052e89cdc119d20b22c3f3
David Anderson <davea@ssl.berkeley.edu>
2025-11-20 01:57:15 -0800

    reorganize

--- examples/wasserklavier.py
+++ examples/wasserklavier.py
@@ -31,81 +31,97 @@ from numula.notate_nuance import *
 from numula.vol_name import *
 import numula.pianoteq
 
-# create the score
-# todo: use absolute pitches here and there
+# Create the score.
+# Divide it into the 6 lines of the printed score.
+# Put each measure on a new line
 #
 def make_score():
-    rh1 = sh_score('3/16 .  d6- 2/8 c 1/8 +c \
-        3/8 -f [c (more -c more) ] \
-        3/16 [ -g 3/8 (more +e- more) ] +d- 2/8 [c (more -f more) ] 1/8 ++c \
-        3/16 [5/8 -f 3/8 (more -g more) ] +d- 2/8  [c (more a- more) ] 1/8 ++c \
-        3/16 -f d- [c -e-] ++c \
+    top1 = sh_score('3/16 .  d6- 2/8 c 1/8 +c \
+        3/8 -f c \
+        3/16 . 3/8 +d- 2/8 c 1/8 ++c \
+        3/16 [5/8 -f] +d- 2/8  c 1/8 ++c \
+        3/16 -f d- [c -e-] c7 \
     ').tag('rh')
-    rh2 = sh_score('3/16 [+c e-] [d- f] d- e- 1/4 c 1/8 c \
+    inner1 = sh_score('6/8 . \
+        2/8 a4- 1/8 b- 2/8 c 1/8 d- \
+        3/8 e- f \
+        g a-
+    ')
+    inner1a = sh_score('6/8 . \
+        2/8 c4 1/8 d- 2/8 e- 1/8 f \
+        3/8 g a- \
+        b- c \
+        b-
+    ')
+    bass1 = sh_score('6/8 . 3/8 f3 f \
+        f f f f f 3/16 f [f5 +d-] \
+    ')
+
+    rh2 = sh_score('3/16 [c6 e-] [d- f] d- e- 1/4 c 1/8 c \
         3/16 [-e- +b-] +b- [--f b- +a-] [d- --b-] 1/4 [c 3/8 +g-] 1/8 -c \
         3/16 [--f +c f] +f [b- f -b-] -e- [a- d- f 1/4 a-] --d- 1/8 _ +a- \
         3/16 [c 3/8 d- g-] -b- d- +c [d- -d- 3/8 g-] [e- +e-] \
     ').tag('rh')
-    rh3 = sh_score('3/16 [++e- 3/8 f -b- f] [+d- +d-] [e- -f c a- f] +++f 1/4 [--f b-] 1/8 [e- -e-] \
+    lh2 = sh_score('3/8 [g5- --f] [+f b- ---f] [+f +e-] \
+        --g- f e- \
+        d- c b- \
+        1/4 e- 1/8 f 1/4 [g- ++b-] 1/8 --a- 3/8 [a ++c] \
+    ')
+
+    rh3 = sh_score('3/16 [e6- 3/8 f -b- f] [+d- +d-] [e- -f c a- f] +++f 1/4 [--f b-] 1/8 [e- -e-] \
         3/16 [-f b- +g-] [+f +f] 1/8 .  [----c +c  c +c] [-c -c] \
         3/16 . [f +e-] c [+c +c] \
        3/16 [ ---f +f f +f] +f ---c [+g- +g-] \
     ').tag('rh')
-    rh4 = sh_score('3/16 +++f b- [--c a-] [++f +c] \
+    lh3 = sh_score(' 3/8 [b4- -b-] 3/16 [c +a-] [d- -d-] 1/4 [c +g-] 1/8 [-c +c] \
+        3/8 [b- f -b-] [a- +a-] \
+        3/16 [g- -g-] ++b- 1/4 c 1/8 [c +c] \
+        3/16 [---f +f] ++d- 3/8 [--e- -e-] \
+    ')
+
+    rh4 = sh_score('3/16 f7 b- [--c a-] [++f +c] \
         [--c -g-]  3/16 [b- +b-  b- +b-] [-e- b- -b-] -e- g- g- \
         [f d-] +d- 1/4 ++d- 1/8 c 3/8 b- \
         1/8 [ ---c e 1/4 a-] [f -a-] 1/4 +g 1/8 [d- +a-] [-e- +d-] \
     ').tag('rh')
-    rh5 = sh_score('1/32 _ 1/64 e- +d- 1/6 [e- +d-] 1/12 c 1/4 [b- -d-] +a- \
+    lh4 = sh_score(' 3/16 [d4- -d-] [f5 +d-] d3 a5- \
+        [e4- -e-] [e6- d-] 3/8 --d- --c \
+        3/16 +c b- 1/4 a- 1/8 [++d- f] 3/16 --g [c ++e] \
+        1/4 --f 1/8 [1/4 f 1/8 +d- +b-]  [c -e-] [-f +f] g \
+    ')
+
+    rh5 = sh_score('1/32 _ 1/64 e5- +d- 1/6 [e- +d-] 1/12 c 1/4 [b- -d-] +a- \
         [b- +g] 1/6 --d- 1/12 c 1/8 [+g +e- +b-] -d- \
         1/6 [d- +a- d-] 1/12 c 1/4 [b- g -c -e-] [d- +b- +f a-] \
         1/8 [g e- -a- -c] ++d- [--b- +g +d- f +d-] c [b- -e- c -f -a-] ++d- \
     ').tag('rh')
-    rh6 = sh_score('1/8 [(more +++d- more) e- +a- d-] c 1/4 [b- g -c a-] [g b- +f a-] \
+    lh5 = sh_score(' 1/8 [f4 a- +f] [-b- +g] [a- -c -f] [+d- +b-] [c -e- -f] [+f  +d-] \
+        [1/4 ---f +f 1/8 ++g +e-] [f -a-] 1/4 [a- --f -f] (less [f +f] \
+        [-f +f] [-f +f] [-f +f] \
+        [-f +f] [-f +f] [-f +f] less) \
+    ')
+
+    rh6 = sh_score('1/8 [(more d7- more) e- +a- d-] c 1/4 [b- g -c a-] [g b- +f a-] \
         [g e- -a- f] [e- g +d- f] [e- c -f -d-] \
         [c e- +b- d-] 1/6 [-d- b-] [+a- c] [-d- b-] \
         1/4 [ (less -f less) c] 3/8 [+b- d- f a-] 1/8 [3/8 --f] \
         1/4 [3/4 a- c] -e- d- \
     ').tag('rh')
-    lh1 = sh_score('6/8 . 2/8 [--f +c (more +a- more) ] 1/8 [-d- (more +b- more) ] 2/8 [--f +e-] 1/8 [f (more +d- more) ] \
-        3/8 --f [f ++a-] \
-        [--f ++b-] [--f ++c] \
-        [--f ++b-] 3/16 --f [++f +d-] \
-    ')
-    lh2 = sh_score('3/8 [+g- --f] [+f b- ---f] [+f +e-] \
-        --g- f e- \
-        d- c b- \
-        1/4 e- 1/8 f 1/4 [g- ++b-] 1/8 --a- 3/8 [a ++c] \
-    ')
-    lh3 = sh_score(' 3/8 [b- -b-] 3/16 [c +a-] [d- -d-] 1/4 [c +g-] 1/8 [-c +c] \
-        3/8 [b- f -b-] [a- +a-] \
-        3/16 [g- -g-] ++b- 1/4 c 1/8 [c +c] \
-        3/16 [---f +f] ++d- 3/8 [--e- -e-] \
-    ')
-    lh4 = sh_score(' 3/16 [-d- -d-] [+++f +d-] ---d +++a- \
-        [--e- -e-] [+++e- d-] 3/8 --d- --c \
-        3/16 +c b- 1/4 a- 1/8 [++d- f] 3/16 --g [c ++e] \
-        1/4 --f 1/8 [1/4 f 1/8 +d- +b-]  [c -e-] [-f +f] g \
-    ')
-    lh5 = sh_score(' 1/8 [-f a- +f] [-b- +g] [a- -c -f] [+d- +b-] [c -e- -f] [+f  +d-] \
-        [1/4 ---f +f 1/8 ++g +e-] [f -a-] 1/4 [a- --f -f] (less [f +f] \
-        [-f +f] [-f +f] [-f +f] \
-        [-f +f] [-f +f] [-f +f] less) \
-    ')
-    lh6 = sh_score(' 1/4 [-f ++g b-] [d- -f -f] [f +e- +c] \
+    lh6 = sh_score(' 1/4 [f4 g5 b-] [d- -f -f] [f +e- +c] \
         [b- -d- -f] [f +c +a-] [g -b- f] \
         [f a- +g] 1/2 [e -g f] \
         3/4 [ (more g more) 6/4 -c -f] +f \
     ')
+
     ns = Score()
-    ns.append_scores([rh1, lh1], 'line1')
+    ns.append_scores([rh1, inner1, inner1a, lh1], 'line1')
     ns.append_scores([rh2, lh2], 'line2')
     ns.append_scores([rh3, lh3], 'line3')
     ns.append_scores([rh4, lh4], 'line4')
     ns.append_scores([rh5, lh5], 'line5')
     ns.append_scores([rh6, lh6], 'line6')
 
-    # add measures (for metric accent)
+    # add measures (for metric accents)
     #
     for i in range(5):
         ns.append_measure(6/8, '6/8')
@@ -119,7 +135,7 @@ def make_score():
         ns.append_measure(3/4, '3/4')
     return ns
 
-#  volume nuance
+#  volume
 
 def set_vol(ns):
     ns.vol_adjust_pft(
@@ -148,7 +164,7 @@ def set_vol(ns):
     ns.vol_adjust(.2, lambda n: 'more' in n.tags, mode=VOL_ADD)
     ns.vol_adjust(.7, lambda n: 'less' in n.tags)
 
-#  timing nuance
+#  timing
 
 def set_tempo(ns):
     ns.tempo_adjust_pft(
