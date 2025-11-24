41feb370e8f076752936df77a7025efe80fcb3ea
davidpanderson <davea@ssl.berkeley.edu>
2025-11-23 16:49:21 -0800

    edits

--- examples/wasserklavier.py
+++ examples/wasserklavier.py
@@ -6,7 +6,7 @@
 # We encode each of these separately,
 # and give them various tags:
 # some of these involve all systems
-#   'bass', 'top_mel'
+#   'bass', 'mel'
 # and others are specific to a system
 #   'inner1', 'inner1a'
 #   'sys1'...'sys6'
@@ -42,7 +42,7 @@ def make_score():
         3/16 . +d- 2/8 c 1/8 +c \
         3/16 [5/8 -f] d- 2/8  c 1/8 +c \
         3/16 -f d- [c -e-] c7 \
-    ').tag('rh')
+    ').tag('mel')
     inner1 = sh_score('6/8 . \
         2/8 a4- 1/8 b- 2/8 c 1/8 d- \
         3/8 e- f \
@@ -56,7 +56,7 @@ def make_score():
     ').tag('more').tag('inner1a')
     bass1 = sh_score('6/8 . 3/8 f3 f \
         f f f f f 3/16 f [f5 +d-] \
-    ')
+    ').tag('bass')
 
     rh2 = sh_score('3/16 [c6 e-] [d- f] d- e- 1/4 c 1/8 c \
         3/16 [-e- +b-] +b- [--f b- +a-] [d- --b-] 1/4 [c 3/8 +g-] 1/8 -c \
@@ -141,7 +141,7 @@ def make_score():
 def set_vol(ns):
     ns.vol_adjust_pft(
         sh_vol('mm 30/8 mm \
-            pp 9/8 ppp 18/8 pp 9/8 p \
+            [ pp 9/8 [ ppp 18/8 [ pp 9/8 p \
             p 27/8 mp \
             [ p 17/8 ppp 15/8 pp \
             pp 24/8 f \
@@ -152,20 +152,20 @@ def set_vol(ns):
 
     # system 1
     ns.vol_adjust_pft(
-        sh_vol('pp 12/8 ppp ] p 9/8 mf ] pp 6/8 ppp ] p 3/8 p'),
-        selector = lambda n: 'mel1' in n.tags
+        sh_vol('pp 12/8 p 9/8 mf ] pp 6/8 ppp ] p 3/8 p'),
+        selector = lambda n: 'mel' in n.tags
     )
     ns.vol_adjust_pft(
-        sh_vol('mm 6/8 mm [ppp 12/8 p 12/8 ppp'),
+        sh_vol('mm 6/8 mm [ ppp 12/8 p 12/8 ppp'),
         selector = lambda n: 'inner1' in n.tags
     )
     ns.vol_adjust_pft(
-        sh_vol('mm 6/8 mm [ppp 12/8 pp 12/8 ppp'),
+        sh_vol('mm 6/8 mm [ ppp 12/8 pp 12/8 ppp'),
         selector = lambda n: 'inner1a' in n.tags
     )
     ns.vol_adjust_pft(
-        sh_vol('mm 6/8 mm [ppp 12/8 p 12/8 ppp'),
-        selector = lambda n: 'bass1' in n.tags
+        sh_vol('mm 6/8 mm [ ppp 12/8 p 12/8 ppp'),
+        selector = lambda n: 'bass' in n.tags
     )
 
     # voice to top/bottom
@@ -200,17 +200,18 @@ def set_tempo(ns):
 
 # pauses/rolls -  do this after overall tempo
 
-# durations of the 6 lines:
-t1 = 30/8
-t2 = 36/8
-t3 = 27/8
-t4 = 30/8
-t5 = 24/8
-t6 = 30/8
+# start times of the 6 systems
+t1 = 0
+t2 = t1 + 30/8
+t3 = t2 + 36/8
+t4 = t3 + 27/8
+t5 = t4 + 30/8
+t6 = t5 + 24/8
+tend = t6 + 30/8
 
-roll4_slow = roller(4, -.4, .2, .8, .05)
-roll4= roller(4, -.2, .1, .8)
-roll3 = roller(3, -.2, .1, .8)
+roll4_slow = roller(4, -.3, .2, .8, .05)
+roll4= roller(4, -.1, .1, .8)
+roll3 = roller(3, -.1, .1, .8)
 roll2 = [-.1, .1]
 
 def ta1(ns):
@@ -238,56 +239,51 @@ def ta1(ns):
     ns.roll(m+3/8, roll3)
 
 def ta2(ns):
-    t = t1
     ns.tempo_adjust_pft(
         sh_tempo('6/8 . p.15 27/8 . p.1'),
-        t1
+        t2
     )
 
 def ta3(ns):
-    t = t1 + t2
     ns.tempo_adjust_pft(
         sh_tempo('6/8 . p.15 3/16 . p.15'),
-        t
+        t3
     )
-    ns.roll(t+3/8, np.linspace(-.3, .1, 7))
-    ns.roll(t+13/8, np.linspace(-.16, .1, 4))
-    ns.roll(t+21/8, np.linspace(-.12, .1, 4), selector=lambda n: 'rh' in n.tags)
+    ns.roll(t3+3/8, np.linspace(-.3, .1, 7))
+    ns.roll(t3+13/8, np.linspace(-.16, .1, 4))
+    ns.roll(t3+21/8, np.linspace(-.12, .1, 4), selector=lambda n: 'rh' in n.tags)
 
 def ta4(ns):
-    t = t1 + t2 + t3
     ns.tempo_adjust_pft(
         sh_tempo('9/8 . p.1 6/8 . p.2'),
-        t
+        t4
     )
-    ns.roll(t+6/8+3/16, np.linspace(-.12, .1, 4), selector=lambda n: 'rh' in n.tags)
-    ns.roll(t+6/8+3/8, np.linspace(-.2, .1, 3), selector=lambda n: 'rh' in n.tags)
+    ns.roll(t4+6/8+3/16, np.linspace(-.12, .1, 4), selector=lambda n: 'rh' in n.tags)
+    ns.roll(t4+6/8+3/8, np.linspace(-.2, .1, 3), selector=lambda n: 'rh' in n.tags)
 
 def ta5(ns):
-    t = t1 + t2 + t3 + t4
-    ns.roll(t+6/8,  np.linspace(-.4, .1, 6))
-    ns.roll(t+14/8, np.linspace(-.2, .1, 4), selector=lambda n: 'rh' in n.tags)
-    ns.roll(t+16/8, np.linspace(-.2, .1, 4), selector=lambda n: 'rh' in n.tags)
-    ns.roll(t+18/8, np.linspace(-.2, .1, 4), selector=lambda n: 'rh' in n.tags)
-    ns.roll(t+20/8, np.linspace(-.2, .1, 5), selector=lambda n: 'rh' in n.tags)
-    ns.roll(t+22/8, np.linspace(-.2, .1, 5), selector=lambda n: 'rh' in n.tags)
+    ns.roll(t5+6/8,  np.linspace(-.4, .1, 6))
+    ns.roll(t5+14/8, np.linspace(-.2, .1, 4), selector=lambda n: 'rh' in n.tags)
+    ns.roll(t5+16/8, np.linspace(-.2, .1, 4), selector=lambda n: 'rh' in n.tags)
+    ns.roll(t5+18/8, np.linspace(-.2, .1, 4), selector=lambda n: 'rh' in n.tags)
+    ns.roll(t5+20/8, np.linspace(-.2, .1, 5), selector=lambda n: 'rh' in n.tags)
+    ns.roll(t5+22/8, np.linspace(-.2, .1, 5), selector=lambda n: 'rh' in n.tags)
 
 def ta6(ns):
-    t = t1 + t2 + t3 + t4 + t5
     ns.tempo_adjust_pft(
         sh_tempo('14/4 . p10'),
-        t
+        t6
     )
     roll1 = roller(8, -.4, .1, .8, .1)
-    ns.roll(t, np.linspace(-.3, .1, 3), selector=lambda n: 'rh' not in n.tags)
-    ns.roll(t+2/8, roll1)
-    ns.roll(t+4/8, roll1)
-    ns.roll(t+6/8, roll1)
-    ns.roll(t+8/8, roll1)
-    ns.roll(t+10/8, roll1)
-    ns.roll(t+12/8, roll1)
-    ns.roll(t+14/8, np.linspace(-.4, .1, 3), selector=lambda n: 'rh' not in n.tags)
-    ns.roll(t+18/8, [-.4, -.3, .2, -.1, -.2])
+    ns.roll(t6, np.linspace(-.3, .1, 3), selector=lambda n: 'rh' not in n.tags)
+    ns.roll(t6+2/8, roll1)
+    ns.roll(t6+4/8, roll1)
+    ns.roll(t6+6/8, roll1)
+    ns.roll(t6+8/8, roll1)
+    ns.roll(t6+10/8, roll1)
+    ns.roll(t6+12/8, roll1)
+    ns.roll(t6+14/8, np.linspace(-.4, .1, 3), selector=lambda n: 'rh' not in n.tags)
+    ns.roll(t6+18/8, [-.4, -.3, .2, -.1, -.2])
 
 def time_adjust(ns):
     ta1(ns)
