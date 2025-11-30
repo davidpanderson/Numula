07f8fd1deab136bf752711fd1b5addfc354abce9
davidpanderson <davea@ssl.berkeley.edu>
2025-11-30 00:32:08 -0800

    edit

--- comp/cycle.py
+++ comp/cycle.py
@@ -7,33 +7,38 @@ def cycle(
     top: PFT,
     incr: PFT,
     vol: PFT,
-    dt
+    dtgen
 ):
     ns = Score()
     t = 0
-    p = bottom.value()
+    p = bottom.value(t)
+    dur = pft_dur(bottom.pft)
 
-    while True:
-        ns.append_note(Note(t, int(p), vol.value(t)))
-        t += dt.next();
+    while t <= dur:
+        dt = next(dtgen)
+        ns.insert_note(Note(t, dt, int(p), vol.value(t)/2))
+        t += dt
         p += incr.value(t)
         if p > top.value(t):
             p = bottom.value(t)
     return ns
 
-def tgen():
+def dtgen():
     while True:
         yield 1/8
         yield 1/8
         yield 1/4
 
 def test_cycle():
+    dts = dtgen()
     ns = cycle(
-        sh_value('40 12/1 30'),
-        sh_value('50 12/1 80'),
-        sh_value('7 12/1 7'),
-        sh_value('mf 12/1 f'),
-        tgen
+        PFTValue(sh_vol('40 12/1 30')),
+        PFTValue(sh_vol('50 12/1 80')),
+        PFTValue(sh_vol('7 12/1 7')),
+        PFTValue(sh_vol('mf 12/1 f')),
+        dts
     )
     print(ns)
+    numula.pianoteq.play_score(ns)
     
+test_cycle()
