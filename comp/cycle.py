52a7ab3fcbf2a240b6b62fb675ac9674ca72e583
davidpanderson <davea@ssl.berkeley.edu>
2025-12-03 13:26:51 -0800

    cycle

--- comp/cycle.py
+++ comp/cycle.py
@@ -1,6 +1,7 @@
 import numula_path
 from numula.nscore import *
 from numula.notate_nuance import *
+from numula.pianoteq import *
 
 def cycle(
     bottom: PFT,
@@ -16,29 +17,36 @@ def cycle(
 
     while t <= dur:
         dt = next(dtgen)
-        ns.insert_note(Note(t, dt, int(p), vol.value(t)/2))
+        ns.insert_note(Note(t, dt*2, int(p), vol.value(t)/2))
         t += dt
-        p += incr.value(t)
+        inc = incr.value(t)
+        bt = bottom.value(t)
+        tp = top.value(t)
+        p += inc
+        diff = tp - bt
         if p > top.value(t):
-            p = bottom.value(t)
+            while p-diff > bt:
+                p -= diff
+        #print('top', tp, 'bottom', bt, 't', t, 'p', p, 'incr', inc)
     return ns
 
 def dtgen():
     while True:
-        yield 1/8
-        yield 1/8
-        yield 1/4
+        for i in range(4):
+            yield 1/32
+        for i in range(3):
+            yield 1/24
 
 def test_cycle():
     dts = dtgen()
     ns = cycle(
-        PFTValue(sh_vol('40 12/1 30')),
-        PFTValue(sh_vol('50 12/1 80')),
-        PFTValue(sh_vol('7 12/1 7')),
-        PFTValue(sh_vol('mf 12/1 f')),
+        PFTValue(sh_vol('45 12/1 40')),
+        PFTValue(sh_vol('55 12/1 80')),
+        PFTValue(sh_vol('7 12/1 31')),
+        PFTValue(sh_vol('pp 6/1 ff 6/1 pp')),
         dts
     )
-    print(ns)
+    #print(ns)
     numula.pianoteq.play_score(ns)
     
 test_cycle()
